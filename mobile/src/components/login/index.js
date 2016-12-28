3a03658588738de7cdb39f9dcc0c3963af92562e
Kuro Souza <kurosouza@gmail.com>
2016-12-28 07:22:03 +0100

    Added @autobinddecorator support (for LoginView)

--- mobile/src/components/login/index.js
+++ mobile/src/components/login/index.js
@@ -2,20 +2,41 @@
 
 import React, {Component} from 'react';
 import {View,Text, TouchableOpacity} from 'react-native';
+import autobind from 'autobind-decorator';
 
 import {Icon, Image, InputGroup, Container, Header, Content, Input, Button, Thumbnail} from 'native-base';
 import styles from './styles';
 
 const logo = require('../../../img/logo.png');
 
+
+
 class Login extends Component {
 
-	constructor() {
-		super();		
+	constructor(props) {
+		super(props);
+		this.state = {email: '', password: ''};		
 	}
 
 	componentDidMount() {
-		this.store = this.props.store;
+		this.store = this.props.store;		
+	}
+
+	loginDetailChanged() {
+		// validate login fields here
+		console.log('login detail changed.');		
+	}
+
+	@autobind
+	emailChanged(email){
+		this.setState({email: email});
+		console.log('email: ' + email);
+	}
+
+	@autobind
+	passwordChanged(password){
+		this.setState({password: password});
+		console.log('password: ' + password);
 	}
 
 	render() {
@@ -26,11 +47,11 @@ class Login extends Component {
 					<Thumbnail source={logo} size={100} style={styles.logo} />
 					<InputGroup style={styles.input}>
 						<Icon name='md-person' size={30} />
-						<Input placeholder="Email" />
+						<Input placeholder="Email" value={this.state.email} onChangeText={this.emailChanged}/>
 					</InputGroup>
 					<InputGroup style={styles.input}>
 						<Icon name='md-lock' size={30} />
-						<Input placeholder="Password" secureTextEntry />
+						<Input placeholder="Password" secureTextEntry value={this.state.password} onChangeText={this.passwordChanged}/>
 					</InputGroup>					
 
 					<Button block success capitalize style={styles.btn}>
